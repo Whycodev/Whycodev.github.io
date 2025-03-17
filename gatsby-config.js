e96e077bf9106ddb75eb502d78140a599f462e02
holyjinny <koreavzz@naver.com>
2025-03-17 22:04:39 +0900

    Gatsby 블로그 초기 구성

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,29 +15,31 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     `gatsby-plugin-image`,
+    // 원본
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // 파일 구조에 따른 수정
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `contents`,
+        path: `${__dirname}/contents`,
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    `gatsby-plugin-sharp`
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,14 +5,69 @@
  */
 
 /**
- * @type {import('gatsby').GatsbyNode['createPages']}
+ * @type {import('gatsby').GatsbyNode['createPages']} // TypeScript가 아닌 환경에서도 VSCode 같은 에디터에서 타입 자동완성을 도와주기위한 부분
  */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+// 원본
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+// const path = require('path');
+
+// exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+//   const output = getConfig().output || {};
+
+//   actions.setWebpackConfig({
+//     output,
+//     resolve: {
+//       alias: {
+//         components: path.resolve(__dirname, 'src/components'),
+//         utils: path.resolve(__dirname, 'src/utils'),
+//         hooks: path.resolve(__dirname, 'src/hooks'),
+//       },
+//     },
+//   });
+// };
+
+// 수정
+// 1. defer: true의 경우 Gatsby Cloud에서만 지원되는데 해당 프로젝트의 경우 gatsby-plugin-gatsby-cloud 패키지를 삭제한 상황이어서 DSG 기능을 사용하지 않는다.
+//    향후 Cloud와 로컬 빌드 모두 고려하여 환경 변수에 따라 처리하기 위해 다음과 같이 명시시
+const isGatsbyCloud = process.env.GATSBY_CLOUD === 'true';
+
+// 1. createPages 함수 비동기 에러 핸들링
+//    만약 페이지 생성 중 에러가 발생하면 프로세스가 중단될 수 있어서, try...catch로 감싸기
+exports.createPages = async ({ actions, reporter }) => {
+  const { createPage } = actions;
+  try {
+    createPage({
+      path: "/using-dsg",
+      component: require.resolve("./src/templates/using-dsg.js"),
+      context: {},
+      defer: isGatsbyCloud,
+    });
+  } catch (error) {
+    reporter.panicOnBuild(`Error while creating pages:`, error);
+  }
+};
+
+const path = require('path');
+
+// 2. getConfig()로 가져온 output은 생략 가능
+//    output은 setWebpackConfig에서 대부분의 경우 자동으로 병합되므로, 굳이 명시적으로 전달할 필요 X
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
