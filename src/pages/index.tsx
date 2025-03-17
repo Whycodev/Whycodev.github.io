eb5b1f1cea7cf3b411bfd5aa7a0a00eb5b649f2f
Whycodev <developer.jinny@gmail.com>
2025-03-17 23:53:41 +0900

    메인 화면 문구 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react'
 import Text from 'components/Text'
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Whycodev's Blog 개발 중입니다." />
+  return <Text text="Whycodev's Blog 개발 중입니다. Gatsby를 활용하여 작업중입니다." />
 }
 
 export default IndexPage
